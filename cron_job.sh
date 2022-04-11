dcb9f241f92e8d58d12b12123cfb1655dfebdc5b
Jen Garcia <jen@haverstack.com>
2022-04-11 16:04:31 -0400

    Add timestamps to cron script

--- cron_job.sh
+++ cron_job.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "----------------------------------"
+echo "Starting Instagram scrape: $(date)"
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd ${SCRIPT_DIR}
 npm run authenticate && npm run scrape
+echo "Finished Instagram scrape: $(date)"
